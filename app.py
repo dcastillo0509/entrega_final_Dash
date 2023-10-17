048d8e2fa02bdf6ea9e6da21b0eaf2438a18dc3a
dcastillo0509 <davidmateo0509@gmail.com>
2023-10-16 21:01:55 -0500

    1version
    avance1

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import plotly.graph_objs as go
 
 #Se importa frontend
 from frontend.main import layout
+from frontend.centro.centro import update_granulometria_table
+from frontend.centro.centro import update_granulometria_plot
+from frontend.centro.centro import generate_granulometria_plot
 
 app=dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 #crea la variable app y agrega __name__ y los estilos con bootstrap
@@ -21,6 +24,23 @@ app.layout = layout #se asigna el layout en la variable layout de app
 
 
 
+@app.callback(
+    Output('tabla_granulometria', 'data'),
+    [Input('tabla_granulometria', 'data'),
+     Input('tabla_granulometria', 'columns')]
+)
+
+def update_tabla_centro(rows, columns):
+    return update_granulometria_table(rows, columns)
+
+
+@app.callback(
+    Output('granulometria-plot', 'figure'),
+    [Input('tabla_granulometria', 'data')]
+)
+
+def update_grafica_centro(rows):
+    return update_granulometria_plot(rows)
 
 
 if __name__ == '__main__': #si __name__ y __main__ son iguales
--- frontend/centro/centro.py
+++ frontend/centro/centro.py
@@ -2,8 +2,10 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 import dash
+import pandas as pd
 import dash_bootstrap_components as dbc
-from dash import html, dcc, dash_table
+from dash import Dash, html, dcc, dash_table, Input, Output, callback
+import plotly.tools as tls
 import plotly.graph_objs as go
 from backend.granulometria import granulometria
 #importa las librerías necesarias
@@ -42,10 +44,53 @@ centro=dbc.Container([
             dcc.Input(id='indicePlasticidad', value="10", type='number', min=7, max=17),
             #se crea un Input con identificador, valor por defecto, tipo numero y rango
             html.Br(),
-])
+],fluid=True)
 
 
 def generate_granulometria_plot():
+    trace = go.Scatter(
+        x=granulometria['Abertura'][0:11],
+        y=granulometria['Por_Pasa'][0:11],
+        mode='lines',
+        line=dict(color='black', width=2),
+        name='Curva Granulométrica'
+    )
+    
+    layout = go.Layout(
+        title='Curva Granulométrica',
+        xaxis=dict(
+            title='Tamiz (mm)',
+            type='log',
+            autorange=True
+        ),
+        yaxis=dict(
+            title='Porcentaje Pasa Acumulado %',
+            range=[0, 100]
+        )
+    )
+
+    return {'data': [trace], 'layout': layout}
+
+
+def update_granulometria_table(rows, columns):
+    granulometria = pd.DataFrame(rows)
+    granulometria["Retenido"] = granulometria["Retenido"].astype("int") 
+    granulometria["Retenido_acum"]= granulometria["Retenido"].cumsum() #se crea una columna para retenido acummulado y se aplica cumsum a la columna retenido para hallar su acumulado
+    granulometria["Pasa"]= granulometria["Retenido"].sum()-granulometria["Retenido_acum"] #Se crea la columna Pasa y se realiza la resta del total de la muestra menos el retenido acumulado en cada fila
+    granulometria["Por_Pasa"]= round(granulometria["Pasa"]*100/granulometria["Retenido"].sum(),2) #Se crea la columna % pasa y se realiza la operació entre la columna pasa por 100 dividido en el total de la muetra
+      
+    # Convertir las columnas numéricas a str para evitar el error
+    granulometria["Retenido"] = granulometria["Retenido"].astype(str)
+    granulometria["Retenido_acum"] = granulometria["Retenido_acum"].astype(str)
+    granulometria["Pasa"] = granulometria["Pasa"].astype(str)
+    granulometria["Por_Pasa"] = granulometria["Por_Pasa"].astype(str)
+         
+    return granulometria.to_dict('records')
+
+
+def update_granulometria_plot(rows):
+    granulometria = pd.DataFrame(rows)
+    
     trace = go.Scatter(
         x=granulometria['Abertura'][0:11],
         y=granulometria['Por_Pasa'][0:11],
