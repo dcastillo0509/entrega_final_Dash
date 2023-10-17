02e089278138c75bca23b62daebd9ee77dd67a46
Dpceballost <143672502+Dpceballost@users.noreply.github.com>
2023-10-16 21:33:59 -0500

    Update granulometria.py

--- backend/granulometria.py
+++ backend/granulometria.py
@@ -64,3 +64,145 @@ granulometria["Pasa"]= granulometria["Retenido"].sum()-granulometria["Retenido_a
 #Se crea la columna Pasa y se realiza la resta del total de la muestra menos el retenido acumulado en cada fila
 granulometria["Por_Pasa"]= round(granulometria["Pasa"]*100/granulometria["Retenido"].sum(),2)
 #Se crea la columna % pasa y se realiza la operació entre la columna pasa por 100 dividido en el total de la muetra
+
+#INTERPOLACIÓN
+# Determinar los diametros para los cuales pasa el 10%, 30% y 60% del material
+from scipy.interpolate import interp1d
+x = granulometria["Porcentaje Pasa"]
+y = abertura
+
+interpolate_x = 10
+y_interp =interp1d(x, y )
+D10=y_interp(interpolate_x)
+
+interpolate_x = 30
+D30=y_interp(interpolate_x)
+
+interpolate_x = 60
+D60=y_interp(interpolate_x)
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Coeficiente de uniformidad (Cu)  y Coeficiente de curvatura (Cc)
+# Calculamos el valor del Cu y Cc
+
+Cu = D60/D10
+print("Cu =",Cu)
+Cc = (D30**2)/(D10*D60)
+print("Cc =",Cc)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Determinar los valores para el tamiz No. 200 y No.4, para hacer la clasificaion del suelo
+
+Pasa_Tamiz_200 = granulometria.loc[granulometria['Malla'] == 'No 200', 'Por_Pasa'].values[0]
+Pasa_Tamiz_4 = granulometria.loc[granulometria['Malla'] == 'No 4', 'Por_Pasa'].values[0]
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# CLASIFICAIÓN DEL SUELO
+print("El TIPO DE SUELO ES = ")
+
+# SUELOS DE PARTICULAS GRUESAS
+if Pasa_Tamiz_200 < 50:
+ print("Suelo de partículas gruesas")
+
+# ARENAS
+ #Si el porcentaje que pasa el tamiz No.4  es mayor o igual al 50% es una arena
+ if Pasa_Tamiz_4 >= 50:
+   print("Es una arena")
+
+# Según el valor del tamiz No.200 se tienen tres tipos de arena
+
+# Arena Limpias 
+   if Pasa_Tamiz_200 < 5:
+    print("Es una arena limpia")
+    if Cu >= 6 and 1<= Cc <= 3:
+       print("Es una arena bien gradada (SW)")
+    if Cu < 6 and 1 > Cc > 3:
+       print("Es una arena mal gradada (SP)")
+
+# Arenas con finos
+   elif Pasa_Tamiz_200 > 12:
+    print(" Arena con finos")
+    if indicePlasticidad < 4: #significa que está debajo de la línea A
+      print("Es una arena arcillosa (SC) Verificar en la carta de plasticidad")
+    if indicePlasticidad > 7: #significa que está arriba de la línea A
+      print("Es una arena limosa (SM) Verificar en la carta de plasticidad")
+ 
+ # Arenas limpias y con finos
+   else:
+    print("Es una arena limpia y con finos")
+#Bien gradada
+    if Cu >= 6 and 1 <= Cc <= 3: 
+      if indicePlasticidad < 4: #significa que está debajo de la línea A
+        print("Es una arena bien gradada con limo (SW-SM) Verificar en la carta de plasticidad")
+      if indicePlasticidad > 7: #significa que está arriba de la línea A
+        print("Es una arena bien gradada con arcilla (SW-SC) Verificar en la carta de plasticidad")
+# Mal gradada
+    else: 
+      if indicePlasticidad < 4: #significa que está debajo de la línea A
+        print("Es una arena mal gradada con limo (SP-SM) Verificar en la carta de plasticidad")
+      if indicePlasticidad > 7: #significa que está arriba de la línea A
+        print("Es una arena mal gradada con arcilla (SP-SC) Verificar en la carta de plasticidad")
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+# SUELOS DE PARTICULAS GRUESAS
+# GRAVAS
+ else:
+   print("Es una grava")
+
+# Según el valor del tamiz No.200 se tienen tres tipos de grava
+
+# Gravas con finos 
+   if Pasa_Tamiz_200 > 12:
+    print("Es una grava con finos")
+    if indicePlasticidad < 4:  #significa que está debajo de la línea A
+     print("Es una grava limosa (GM) Verificar en la carta de plasticidad")
+    if indicePlasticidad > 7: #significa que está arriba de la línea A
+     print("Es una grava Arcillosa (GC) Verificar en la carta de plasticidad")
+    
+# Gravas limpias
+   elif Pasa_Tamiz_200 < 5:
+     print("Es una grava limpia")
+     if Cu >= 4 and 1 <= Cc <= 3:
+      print("Es una Grava bien gradada (GW)")
+     if Cu < 4 and 1 > Cc > 3:
+      print("Es una Grava mal gradada (GP)")
+  
+# Gravas limpias y con finos
+   else:
+     print("Es una grava limpia y con finos")
+
+    # Segun el Cu y el Cc se clasifican las arenas limpias y con finos 
+
+    # Bien gradada
+   if Cu >= 4 and 1 <= Cc <= 3:
+    if indicePlasticidad < 4: #significa que está debajo de la línea A
+     print("Es una grava bien gradada con limo (GW-GM) Verificar en la carta de plasticidad")
+    if indicePlasticidad > 7: #significa que está arriba de la línea A
+     print("Es una grava bien gradada con arcilla (GW-GC) Verificar en la carta de plasticidad")
+
+    # Mal gradada
+   else:
+    if indicePlasticidad < 4: #significa que está debajo de la línea A
+     print("Es una grava mal gradada con limo (GP-GM) Verificar en la carta de plasticidad")
+    if indicePlasticidad > 7: #significa que está arriba de la línea A
+     print("Es una grava mal gradada con arcilla (GP-GC) Verificar en la carta de plasticidad")
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+# SUELOS DE PARTICULAS FINAS
+else:
+ print("Suelo de partículas finas")
+ if limliquido > 50:
+   # Inorgánicos de alta plasticidad
+   print("El suelo puede ser una arcilla o un limo de alta plasticidad")
+   if indicePlasticidad < 4: #significa que está debajo de la línea A
+     print("Es un limo de alta plasticidad (MH) Verificar en la carta de plasticidad")
+   if indicePlasticidad > 7: #significa que está arriba de la línea A
+     print("Es una arcilla de alta plasticidad (CH) Verificar en la carta de plasticidad")
+
+
+   # Inorgánicos de baja plasticidad
+ else:
+  print("El suelo puede ser una arcilla o un limo de baja plasticidad")
+  if indicePlasticidad < 4: #significa que está debajo de la línea A
+     print("Es un limo de baja plasticidad (ML) Verificar en la carta de plasticidad")
+  if indicePlasticidad > 7: #significa que está arriba de la línea A
+     print("Es una arcilla de baja plasticidad (CL) Verificar en la carta de plasticidad")
